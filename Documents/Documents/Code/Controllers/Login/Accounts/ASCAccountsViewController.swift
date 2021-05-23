b8cd52a60f689d696bbfe0ba02907bbc3cc69ef4
Ivan Grishechko <ivan.grishechko@onlyoffice.com>
2021-05-23 17:56:43 +0300

    Fixed bug #49816

--- Documents/Documents/Code/Controllers/Login/Accounts/ASCAccountsViewController.swift
+++ Documents/Documents/Code/Controllers/Login/Accounts/ASCAccountsViewController.swift
@@ -33,6 +33,10 @@ class ASCAccountsViewController: ASCBaseViewController {
         
         setupLayout()
         currentPage = 0
+        
+        self.view.subviews
+                .filter { $0 is UIButton }
+                .forEach { $0.isExclusiveTouch = true }
     }
     
     deinit {

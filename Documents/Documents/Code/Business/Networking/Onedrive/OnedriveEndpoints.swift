8660d3102817d47a19ce904873a17b4e56e8602c
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2021-06-24 13:47:26 +0300

    Fixed read onedrive user info

--- Documents/Documents/Code/Business/Networking/Onedrive/OnedriveEndpoints.swift
+++ Documents/Documents/Code/Business/Networking/Onedrive/OnedriveEndpoints.swift
@@ -11,13 +11,11 @@ import Foundation
 class OnedriveAPI {
 
     struct Path {
-        static public let currentAccount = "users"
-        static public let temporaryLink = "drive"
         static public let me = "me"
     }
 
     struct Endpoints {
-        static let me: Endpoint<ASCUser> = Endpoint<DropboxAccount>.make(Path.me)
+        static let me: Endpoint<ASCUser> = Endpoint<ASCUser>.make(Path.me)
     }
 
 }

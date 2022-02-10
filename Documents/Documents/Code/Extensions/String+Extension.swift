f8946603b70867b361311781d94f3548056a478c
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2022-02-10 12:45:47 +0300

    Fix md5 property of String

--- Documents/Documents/Code/Extensions/String+Extension.swift
+++ Documents/Documents/Code/Extensions/String+Extension.swift
@@ -247,14 +247,12 @@ extension String {
     ///     "Hello".md5 -> Optional("8b1a9953c4611296a827abf8c47804d7")
     ///
     var md5: String? {
-        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
-
-        let hash = data.withUnsafeBytes { (bytes: UnsafePointer<Data>) -> [UInt8] in
-            var hash: [UInt8] = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes, CC_LONG(data.count), &hash)
+        let data = Data(self.utf8)
+        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
             return hash
         }
-
         return hash.map { String(format: "%02x", $0) }.joined()
     }
 

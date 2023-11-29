475c62393e7ef6764120d7182c518aff3e5257e8
LolaChernysheva <lolita.chernysheva@onlyoffice.com>
2023-11-29 16:48:01 +0700

    Link life time screen

---
+++ Documents/Documents/Code/Controllers/Links/LinkLifeTime/ASCDocSpaceLinkLifeTimeView.swift
@@ -0,0 +1,68 @@
+//
+//  ASCDocSpaceLinkLifeTimeView.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 28.11.2023.
+//  Copyright © 2023 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ASCDocSpaceLinkLifeTimeView: View {
+    @ObservedObject private var viewModel = ASCDocSpaceLinkLifeTimeViewModel()
+
+    var body: some View {
+        NavigationView {
+            list
+                .navigationBarTitle(Text(NSLocalizedString("Link life time", comment: "")), displayMode: .inline)
+                .navigationBarItems(leading: Button(action: {
+                    // MARK: TODO add back btn action
+                }, label: {
+                    backButtom
+                }))
+        }
+    }
+    
+    private var list: some View {
+        List($viewModel.linkLifeTimeModels) { model in
+            LinkLifeOptionsCell(model: model) { item in
+                viewModel.select(linkLifeTimeModel: item)
+            }
+        }
+    }
+    
+    private var backButtom: some View {
+        HStack {
+            Image(systemName: "chevron.left")
+                .foregroundColor(Asset.Colors.brend.swiftUIColor)
+            Text(NSLocalizedString("Back", comment: ""))
+                .foregroundColor(Asset.Colors.brend.swiftUIColor)
+        }
+    }
+}
+
+struct LinkLifeOptionsCell: View {
+    @Binding var model: LinkLifeTimeModel
+    var tapAction: (LinkLifeTimeModel) -> Void
+    
+    var body: some View {
+        HStack {
+            Text(model.option.localized)
+            Spacer()
+            if model.selected {
+                Image(systemName: "checkmark")
+                    .foregroundColor(Asset.Colors.brend.swiftUIColor)
+            }
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            tapAction(model)
+        }
+    }
+}
+
+struct ASCDocSpaceLinkLifeTimeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ASCDocSpaceLinkLifeTimeView()
+    }
+}
---
+++ Documents/Documents/Code/Controllers/Links/LinkLifeTime/ASCDocSpaceLinkLifeTimeViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ASCDocSpaceLinkLifeTimeViewModel.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 29.11.2023.
+//  Copyright © 2023 Ascensio System SIA. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+enum LinkLifeTimeOption: String, CaseIterable {
+    case twelveHours = "12 hours"
+    case oneDay = "1 day"
+    case sevenDays = "7 days"
+    case unlimited = "Unlimited"
+    case custom = "Custom"
+
+    var localized: String {
+        NSLocalizedString(rawValue, comment: "")
+    }
+}
+
+struct LinkLifeTimeModel: Identifiable {
+    var id: String { option.rawValue }
+    var option: LinkLifeTimeOption
+    var selected: Bool
+}
+
+final class ASCDocSpaceLinkLifeTimeViewModel: ObservableObject {
+    @Published var linkLifeTimeModels: [LinkLifeTimeModel] = []
+    
+    init() {
+        linkLifeTimeModels = LinkLifeTimeOption.allCases.map {
+            // TODO: Check default selected
+            LinkLifeTimeModel(option: $0, selected: false)
+        }
+    }
+    
+    func select(linkLifeTimeModel: LinkLifeTimeModel) {
+        linkLifeTimeModels.enumerated().forEach { index, item in
+            linkLifeTimeModels[index].selected = linkLifeTimeModel.id == item.id
+        }
+    }
+}

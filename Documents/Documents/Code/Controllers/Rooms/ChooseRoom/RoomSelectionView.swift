659b3f0904ac915023d7d5f11ad9dfdb238d7385
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2023-11-30 23:38:11 +0700

    [ds] decompose roomView and fix preview

--- Documents/Documents/Code/Controllers/Rooms/ChooseRoom/RoomSelectionView.swift
+++ Documents/Documents/Code/Controllers/Rooms/ChooseRoom/RoomSelectionView.swift
@@ -20,29 +20,33 @@ struct RoomSelectionView: View {
 
     var body: some View {
         List(viewModel.rooms, id: \.name) { room in
-            HStack {
-                Image(systemName: room.icon)
-                    .foregroundColor(.accentColor)
-                    .frame(width: 36, height: 36)
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(8)
-
-                VStack(alignment: .leading) {
-                    Text(room.name)
-                        .font(.headline)
-                    Text(room.description)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
+            roomView(room)
+                .padding(.vertical, 4)
+                .onTapGesture {
+                    viewModel.selectRoom(room)
+                    isCreateRoomPresenting = true
                 }
-            }
-            .padding(.vertical, 4)
-            .onTapGesture {
-                viewModel.selectRoom(room)
-                isCreateRoomPresenting = true
-            }
         }
         .navigation(isActive: $isCreateRoomPresenting) {
             CreateRoomView()
+        }
+    }
+
+    private func roomView(_ room: Room) -> some View {
+        HStack {
+            Image(systemName: room.icon)
+                .foregroundColor(.accentColor)
+                .frame(width: 36, height: 36)
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(8)
+
+            VStack(alignment: .leading) {
+                Text(room.name)
+                    .font(.headline)
+                Text(room.description)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
         }
     }
 }
@@ -50,17 +54,6 @@ struct RoomSelectionView: View {
 struct RoomSelectionView_Previews: PreviewProvider {
     static var previews: some View {
         let view = RoomSelectionView()
-        view.viewModel.rooms = [
-            Room(name: "Collaboration room",
-                 description: "Collaborate on one or multiple documents with your team",
-                 icon: "bolt.fill"),
-            Room(name: "Public room",
-                 description: "Invite users via shared links to view documents without registration. You can also embed this room into any web interface.",
-                 icon: "person.crop.circle.fill"),
-            Room(name: "Custom room",
-                 description: "Apply your own settings to use this room for any custom purpose",
-                 icon: "star.fill"),
-        ]
         return view
     }
 }

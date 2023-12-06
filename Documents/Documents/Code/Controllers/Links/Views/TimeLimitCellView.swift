3c8bfaecbd28588d0097127eb08b77dfe3f8eb04
LolaChernysheva <lolita.chernysheva@onlyoffice.com>
2023-12-06 16:58:59 +0700

    Add time limit cell

---
+++ Documents/Documents/Code/Controllers/Links/Views/TimeLimitCellView.swift
@@ -0,0 +1,31 @@
+//
+//  TimeLimitCellView.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 05.12.2023.
+//  Copyright © 2023 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimeLimitCellView {
+    @State var model: TimeLimitCellModel
+    var body: some View {
+        HStack {
+            DatePicker(
+                model.title,
+                selection: $model.selectedDate,
+                in: Date()...,
+                displayedComponents: .date
+            )
+            .datePickerStyle(.automatic)
+        }
+    }
+}
+
+struct TimeLimitCellModel {
+    @State var selectedDate: Date = Calendar.current.date(byAdding: .day, value: 7, to: Date()) ?? Date()
+    var title = ""
+}
+
+

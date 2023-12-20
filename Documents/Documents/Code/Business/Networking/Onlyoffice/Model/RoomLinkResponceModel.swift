e406acc26255665cbb4e763ea78af40ce313be3a
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2023-12-21 00:38:32 +0700

    [ds] SharedTo response model

--- Documents/Documents/Code/Business/Networking/Onlyoffice/Model/RoomLinkResponceModel.swift
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/RoomLinkResponceModel.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+    struct SharedTo: Codable {
+        let id: String
+        let title: String
+        let shareLink: String
+        let expirationDate: String?
+        let linkType: Int
+        let password: String?
+        let denyDownload: Bool
+        let isExpired: Bool
+        let primary: Bool
+        let requestToken: String
+
+        enum CodingKeys: String, CodingKey {
+            case id, title, shareLink, expirationDate, linkType, password, denyDownload, isExpired, primary, requestToken
+        }
+    }

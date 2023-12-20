82c71a33ffff4877cea0e3b941bc8bdac723df57
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2023-12-21 00:36:23 +0700

    [ds] RoomLinkRequestModel

---
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/Request/RoomLinkRequestModel.swift
@@ -0,0 +1,18 @@
+//
+//  RoomLinkRequestModel.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 19.12.2023.
+//  Copyright © 2023 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+
+struct RoomLinkRequestModel: Codable {
+    
+    var title: String
+    var access: Int
+    var expirationDate: String
+    var linkType: Int
+    var denyDownload: Bool
+}
---
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/RoomLinkResponceModel.swift
@@ -0,0 +1,9 @@
+//
+//  RoomLinkRepnceModel.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 21.12.2023.
+//  Copyright © 2023 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation

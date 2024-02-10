a71dd102a5de8ffbb2d53496704bcb043002611b
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-02-10 22:05:12 +0700

    upload assets

---
+++ Documents/Documents/Code/Extensions/TimeInterval+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  TimeInterval+Extensions.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 10.02.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation

2dcaf2cb25dcbbbb69b8888bd48ef843a16938bc
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-07-02 19:54:15 +0100

    present sheet with completion selected cloud

--- Documents/Documents/Code/Controllers/ConnectStorage/ASCConnectCloudViewControllerRepresentable.swift
+++ Documents/Documents/Code/Controllers/ConnectStorage/ASCConnectCloudViewControllerRepresentable.swift
@@ -9,9 +9,33 @@
 import SwiftUI
 
 struct ASCConnectCloudViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    var completion: (ASCFileProviderProtocol) -> Void
+    
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
     
     func makeUIViewController(context: Context) -> some UIViewController {
-        return ASCConnectCloudViewController.instantiate(from: Storyboard.connectStorage)
+        let connectStorageVC = ASCConnectCloudViewController.instantiate(from: Storyboard.connectStorage)
+        let navigationVC = UINavigationController(rootViewController: connectStorageVC)
+        
+        if #available(iOS 15.0, *) {
+            if let sheet = navigationVC.sheetPresentationController {
+                sheet.detents = [.medium(), .large()]
+                sheet.largestUndimmedDetentIdentifier = .large
+                sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+                sheet.prefersEdgeAttachedInCompactHeight = true
+                sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+            }
+        } else {
+            navigationVC.modalPresentationStyle = .pageSheet
+        }
+        
+        connectStorageVC.dismissOnCompetion = false
+        connectStorageVC.complation = { provider in
+            completion(provider)
+            navigationVC.navigationController?.popToRootViewController(animated: false)
+        }
+        
+        return navigationVC
     }
 }

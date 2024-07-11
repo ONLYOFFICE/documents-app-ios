5e18240bba904dbed00a7edee49273bf549ff05b
Victor Tihovodov <viktor.tihovodov@onlyoffice.com>
2024-07-11 15:38:34 +0300

    Small change in ASCYandexManager

--- Documents/Documents/Code/Controllers/ConnectStorage/ASCYandexManager.swift
+++ Documents/Documents/Code/Controllers/ConnectStorage/ASCYandexManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 import YandexLoginSDK
 
-public class ASCYandexManager {
+public final class ASCYandexManager {
     static func activateYandexLoginSDK() {
         do {
             let clientID = ASCConstants.Clouds.Yandex.appId

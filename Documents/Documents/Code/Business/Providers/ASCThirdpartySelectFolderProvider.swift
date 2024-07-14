d03f0fcdeaabe614bb3715f01be8266bdbea2bcc
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-07-14 22:39:31 +0100

    add ASCThirdpartySelectFolderProvider to handle and manage selecting folders

---
+++ Documents/Documents/Code/Business/Providers/ASCThirdpartySelectFolderProvider.swift
@@ -0,0 +1,97 @@
+//
+//  ASCThirdpartySelectFolderProvider.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 14.07.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class ASCThirdpartySelectFolderProvider: ASCFileProviderProtocol {
+    
+    var id: String?
+    
+    var type: ASCFileProviderType
+    
+    var rootFolder: ASCFolder
+    
+    var user: ASCUser?
+    
+    var items: [ASCEntity] = []
+    
+    var page: Int = 0
+    
+    var total: Int = 0
+    
+    var authorization: String?
+    
+    var delegate: (any ASCProviderDelegate)?
+    
+    var filterController: (any ASCFiltersControllerProtocol)?
+    
+    init(rootFolder: ASCFolder, type: ASCFileProviderType) {
+        self.rootFolder = rootFolder
+        self.type = type
+    }
+    
+    func copy() -> any ASCFileProviderProtocol {
+        let copy = ASCThirdpartySelectFolderProvider(
+            rootFolder: rootFolder,
+            type: type
+        )
+
+        copy.items = items.map { $0 }
+        copy.page = page
+        copy.total = total
+        copy.delegate = delegate
+        copy.authorization = authorization
+
+        return copy
+    }
+    
+    func reset() {
+        total = 0
+        items.removeAll()
+    }
+    
+    func add(item: ASCEntity, at index: Int) {
+        if !items.contains(where: { $0.uid == item.uid }) {
+            items.insert(item, at: index)
+            total += 1
+        }
+    }
+
+    func add(items: [ASCEntity], at index: Int) {
+        let uniqItems = items.filter { item -> Bool in
+            !self.items.contains(where: { $0.uid == item.uid })
+        }
+        self.items.insert(contentsOf: uniqItems, at: index)
+        total += uniqItems.count
+    }
+
+    func remove(at index: Int) {
+        items.remove(at: index)
+        total -= 1
+    }
+    
+    func fetch(for folder: ASCFolder, parameters: [String : Any?], completeon: ASCProviderCompletionHandler?) {
+        OnlyofficeApiClient.request(OnlyofficeAPI.Endpoints.Folders.path(of: folder)) { [self] response, error in
+            
+        }
+    }
+}
+
+// MARK: - Unsupported funcs
+
+extension ASCThirdpartySelectFolderProvider {
+    
+    func open(file: ASCFile, openMode: ASCDocumentOpenMode, canEdit: Bool) {
+        log.error(#function, " doesn't supported")
+    }
+    
+    func preview(file: ASCFile, files: [ASCFile]?, in view: UIView?) {
+        log.error(#function, " doesn't supported")
+    }
+}

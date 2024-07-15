d76259d73a6c4b87ee8094ac59c2bcb508d6148f
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-07-15 19:56:28 +0100

    create files chevron right view and transfer vc represented controller

---
+++ Documents/Documents/Code/Controllers/Transfer/ASCTransferViewControllerRepresentable.swift
@@ -0,0 +1,9 @@
+//
+//  ASCTransferViewControllerRepresentable.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 15.07.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
---
+++ Documents/Documents/Code/SwiftUIViews/ChevronRightView.swift
@@ -0,0 +1,9 @@
+//
+//  ChevronRightView.swift
+//  Documents
+//
+//  Created by Pavel Chernyshev on 15.07.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation

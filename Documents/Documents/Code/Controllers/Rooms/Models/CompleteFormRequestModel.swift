8781721f1ddb4c135e515aa7f8123165ee70d72c
LolaChernysheva <lolita.chernysheva@onlyoffice.com>
2024-09-19 21:36:17 +0100

    Add models for complete form request

---
+++ Documents/Documents/Code/Controllers/Rooms/Models/CompleteFormRequestModel.swift
@@ -0,0 +1,14 @@
+//
+//  CompleteFormRequestModel.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 19.09.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+
+struct CompleteFormRequestModel: Codable {
+    let fillingSessionId: String
+}
+
---
+++ Documents/Documents/Code/Controllers/Rooms/Models/CompletedFormResponceModel.swift
@@ -0,0 +1,14 @@
+//
+//  CompletedFormResponceModel.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 18.09.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+
+struct CompletedFormResponceModel: Codable {
+    let formNumber: Int
+    let roomId: Int
+}

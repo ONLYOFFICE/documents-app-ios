b6588ff413782a78dc8fd1fa0423f47a9ff2d33c
Alexander Yuzhin <alexander.yuzhin@onlyoffice.com>
2024-09-23 16:05:23 +0300

    Fix headers

--- Documents/Documents/Code/Controllers/Documents/Menu/FillFormMenu/FillFormMenuScreenRepresentable.swift
+++ Documents/Documents/Code/Controllers/Documents/Menu/FillFormMenu/FillFormMenuScreenRepresentable.swift
@@ -1,6 +1,6 @@
 //
 //  FillFormMenuScreenRepresentable.swift
-//  Documents-opensource
+//  Documents
 //
 //  Created by Pavel Chernyshev on 13.09.2024.
 //  Copyright © 2024 Ascensio System SIA. All rights reserved.

e8be0af287f0bdd7f90b988ff881170d95bfd0f1
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-11-28 01:31:24 +0000

    Toggle button view model

--- Documents/Documents/Code/SwiftUIViews/ToggleButtonView.swift
+++ Documents/Documents/Code/SwiftUIViews/ToggleButtonView.swift
@@ -10,3 +10,26 @@ import SwiftUI
 
 struct ToggleButtonView: View {
 }
+
+// MARK: - View Model
+
+extension ToggleButtonView {
+    struct ViewModel: Identifiable, Hashable {
+        let id: String
+        let title: String
+        var isActive: Bool
+        let tapHandler: (String) -> Void
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+            hasher.combine(title)
+            hasher.combine(isActive)
+        }
+
+        static func == (lhs: Self, rhs: Self) -> Bool {
+            lhs.id == rhs.id
+                && lhs.title == rhs.title
+                && lhs.isActive == rhs.isActive
+        }
+    }
+}

d3cda3c63832100e34338e38d423b3cd2a95c583
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-11-28 01:36:04 +0000

    Collection layout

--- Documents/Documents/Code/SwiftUIViews/ToggleButtonCollectionView.swift
+++ Documents/Documents/Code/SwiftUIViews/ToggleButtonCollectionView.swift
@@ -10,6 +10,25 @@ import SwiftUI
 
 struct ToggleButtonCollectionView: View {
     typealias ButtonModel = ToggleButtonView.ViewModel
+
+    let buttonModels: [ButtonModel]
+    let width: CGFloat
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(buttonRows, id: \.hashValue) { row in
+                HStack(spacing: .spaceBetweenItems) {
+                    ForEach(row, id: \.hashValue) { button in
+                        ToggleButtonView(model: button)
+                            .contentShape(Rectangle())
+                    }
+                }
+                .contentShape(Rectangle())
+                .padding(.top, 4)
+            }
+        }
+    }
+
     private var buttonRows: [[ButtonModel]] {
         var rows: [[ButtonModel]] = []
         var currentRow: [ButtonModel] = []

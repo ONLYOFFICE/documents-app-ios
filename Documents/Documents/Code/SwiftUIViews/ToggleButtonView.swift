cb7744df3602384e5582257532b94f46b273af9c
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-11-28 01:29:43 +0000

    Create ToggleButtonView file

---
+++ Documents/Documents/Code/SwiftUIViews/ToggleButtonView.swift
@@ -0,0 +1,12 @@
+//
+//  ToggleButtonView.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 28.11.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ToggleButtonView: View {
+}

b22624031d7f049912cc7100213e53caaa6776d8
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-11-28 01:35:43 +0000

    Collection distribute buttons on rows depends on fitting

--- Documents/Documents/Code/SwiftUIViews/ToggleButtonCollectionView.swift
+++ Documents/Documents/Code/SwiftUIViews/ToggleButtonCollectionView.swift
@@ -9,7 +9,30 @@
 import SwiftUI
 
 struct ToggleButtonCollectionView: View {
-    
+    typealias ButtonModel = ToggleButtonView.ViewModel
+    private var buttonRows: [[ButtonModel]] {
+        var rows: [[ButtonModel]] = []
+        var currentRow: [ButtonModel] = []
+        var remainingWidth = width
+
+        for button in buttonModels {
+            let buttonWidth = ToggleButtonView.calculateWidth(for: button.title) + .spaceBetweenItems
+            if buttonWidth > remainingWidth {
+                rows.append(currentRow)
+                currentRow = [button]
+                remainingWidth = width - buttonWidth
+            } else {
+                currentRow.append(button)
+                remainingWidth -= buttonWidth
+            }
+        }
+
+        if !currentRow.isEmpty {
+            rows.append(currentRow)
+        }
+
+        return rows
+    }
 }
 
 // MARK: - Constants

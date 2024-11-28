852acf384b6f33166be4adc3058b1b80b8cc2e6b
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-11-28 01:33:36 +0000

    Toggle buttons collection file

---
+++ Documents/Documents/Code/SwiftUIViews/ToggleButtonCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  ToggleButtonCollectionView.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 28.11.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ToggleButtonCollectionView: View {
+    
+}

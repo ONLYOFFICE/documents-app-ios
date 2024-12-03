53bfaeea2a472b2b3b70a2c9c5f648f8a2ad4692
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-12-03 21:58:53 +0000

    watermark image editor

---
+++ Documents/Documents/Code/Controllers/Rooms/CreateRoomFlow/ManageRoom/Subviews/WatermarkImageEditorView.swift
@@ -0,0 +1,13 @@
+//
+//  WatermarkImageEditorView.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 02.12.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct WatermarkImageEditorView: View {
+    
+}

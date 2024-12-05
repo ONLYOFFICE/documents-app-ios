37df0004c4e49238a778c5d18027d1a3f4322dfb
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2024-12-05 23:14:37 +0000

    room quota network service

---
+++ Documents/Documents/Code/Controllers/Rooms/CreateRoomFlow/Services/RoomQuotaNetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  RoomQuotaNetworkService.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 05.12.2024.
+//  Copyright © 2024 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+
+final class RoomQuotaNetworkService {
+    private var networkService = OnlyofficeApiClient.shared
+    
+
+}

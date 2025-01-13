efdebeba1a92f1da068cee4d1ec68053a570be66
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-01-13 02:00:20 +0000

    change drag and drop order request model

---
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/Request/FilesOrderRequesModel.swift
@@ -0,0 +1,32 @@
+//
+//  FilesOrderRequesModel.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 13.01.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import Alamofire
+
+struct FilesOrderRequestModel: Codable {
+    var items: [FilesOrderItemRequestModel]
+}
+
+struct FilesOrderItemRequestModel: Codable {
+    var order: String
+    var entryId: Int
+    var entryType: Int
+}
+
+extension Array where Element == ASCEntity {
+    var filesOrderRequestModel: FilesOrderRequestModel {
+        let items = map {
+            FilesOrderItemRequestModel(
+                order: $0.orderIndex ?? "",
+                entryId: Int($0.id) ?? 0,
+                entryType: $0 is ASCFolder ? 1 : 2
+            )
+        }
+        return FilesOrderRequestModel(items: items)
+    }
+}

efca76db86f95ffb48f87f533267c4a2cabcf7f8
LolaChernysheva <lolita.chernysheva@onlyoffice.com>
2025-04-23 14:48:35 +0100

    Delete version request

--- Documents/Documents/Code/Controllers/VersionHistory/ASCVersionHistoryNetworkService.swift
+++ Documents/Documents/Code/Controllers/VersionHistory/ASCVersionHistoryNetworkService.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2025 Ascensio System SIA. All rights reserved.
 //
 
+import Foundation
 
 protocol ASCVersionHistoryNetworkServiceProtocol {
     func restoreVersion(file: ASCFile, versionNumber: Int, completion: @escaping (Result<ASCFile, Error>) -> Void)
     func loadData(file: ASCFile, completion: @escaping (Result<[ASCFile], Error>) -> Void)
-    
+    func deleteVersion(file: ASCFile, versionNember: Int, handler: ASCEntityProgressHandler?)
 }
 
 final class ASCVersionHistoryNetworkService: ASCVersionHistoryNetworkServiceProtocol {
@@ -44,6 +45,42 @@ final class ASCVersionHistoryNetworkService: ASCVersionHistoryNetworkServiceProt
             }
             completion(.success(versions))
             
+        }
+    }
+    
+    func deleteVersion(file: ASCFile, versionNember: Int, handler: ASCEntityProgressHandler?) {
+        var cancel = false
+        
+        guard let fileId = Int(file.id) else { return }
+        handler?(.begin, 0, nil, nil, &cancel)
+        
+        let requestModel = ASCDeleteVersionRequestModel(fileId: fileId, versions: [versionNember])
+        
+        networkService.request(OnlyofficeAPI.Endpoints.Operations.deleteVersion, requestModel.dictionary) { responce, error in
+            if let error = error {
+                handler?(.error, 1, nil, error, &cancel)
+            } else {
+                var checkOperation: (() -> Void)?
+                checkOperation = {
+                    self.networkService.request(OnlyofficeAPI.Endpoints.Operations.list) { result, error in
+                        if let error = error {
+                            handler?(.error, 1, nil, error, &cancel)
+                        } else if let operation = result?.result?.first,
+                                  let progress = operation.progress {
+                            if progress >= 100 {
+                                handler?(.end, 1, nil, nil, &cancel)
+                            } else {
+                                Thread.sleep(forTimeInterval: 1)
+                                checkOperation?()
+                            }
+                        } else {
+                            handler?(.error, 1, nil, NetworkingError.invalidData, &cancel)
+                        }
+                        
+                    }
+                }
+                checkOperation?()
+            }
         }
     }
 }

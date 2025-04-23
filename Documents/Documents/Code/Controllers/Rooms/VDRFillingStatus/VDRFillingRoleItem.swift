6dd4868a978e030e1151d94c4a73e0d6d28d124a
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-04-23 18:28:48 +0100

    role item model

---
+++ Documents/Documents/Code/Controllers/Rooms/VDRFillingStatus/VDRFillingRoleItem.swift
@@ -0,0 +1,14 @@
+//
+//  VDRFillingRoleItem.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 23.04.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+struct VDRFillingRoleItem: Identifiable {
+    let id = UUID()
+    let number: Int
+    let title: String
+    let color: Color
+}

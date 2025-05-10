c300a972bb85ce831f926698b0e6c41c848ff925
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-05-10 10:44:16 +0100

    [docspace 3.1] VDR filling status card style modifier

---
+++ Documents/Documents/Code/SwiftUIViewModifiers/CardStyle.swift
@@ -0,0 +1,20 @@
+//
+//  CardStyle.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 7.05.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardStyle: ViewModifier {
+    var cornerRadius: CGFloat = 16
+    var background: Color = .white
+    func body(content: Content) -> some View {
+        content
+            .background(background)
+            .cornerRadius(cornerRadius)
+            .shadow(color: Color.black.opacity(0.05), radius: 4, x: 0, y: 2)
+    }
+}

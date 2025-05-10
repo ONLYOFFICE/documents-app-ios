eef5f9610dfd05e035282353e4e7d28d8dcb6b6e
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-05-10 10:44:40 +0100

    [docspace 3.1] VDR filling status header view

---
+++ Documents/Documents/Code/Controllers/Rooms/VDRFillingStatus/Subviews/VDRFillingStatusHeaderView.swift
@@ -0,0 +1,29 @@
+//
+//  VDRFillingStatusHeaderView.swift
+//  Documents-opensource
+//
+//  Created by Pavel Chernyshev on 6.05.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import SwiftUI
+
+/// Header with title and cancel action
+struct VDRFillingStatusHeaderView: View {
+    let title: String
+    let onCancel: () -> Void
+
+    var body: some View {
+        HStack {
+            Button("Cancel", action: onCancel)
+                .foregroundColor(.blue)
+            Spacer()
+            Text(title)
+                .font(.headline)
+            Spacer()
+            Color.clear.frame(width: 60, height: 1)
+        }
+        .padding()
+        .background(Color.white)
+    }
+}

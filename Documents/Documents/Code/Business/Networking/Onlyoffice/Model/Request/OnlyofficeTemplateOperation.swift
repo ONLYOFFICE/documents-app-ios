f46df354d9bcd83317c8447df2e816d08955ecb4
LolaChernysheva <lolita.chernysheva@onlyoffice.com>
2025-05-26 15:02:53 +0100

    Add template operation

---
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/Request/OnlyofficeTemplateOperation.swift
@@ -0,0 +1,27 @@
+//
+//  OnlyofficeTemplateOperation.swift
+//  Documents
+//
+//  Created by Lolita Chernysheva on 26.05.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+final class OnlyofficeTemplateOperation: Mappable {
+    
+    var templateId: Int?
+    var progress: Int?
+    var error: String?
+    var isCompleted: Bool = false
+    
+    required init?(map: Map) {}
+    
+    func mapping(map: Map) {
+        templateId <- map["templateId"]
+        progress <- map["progress"]
+        error <- map["error"]
+        isCompleted <- map["isCompleted"]
+    }
+}

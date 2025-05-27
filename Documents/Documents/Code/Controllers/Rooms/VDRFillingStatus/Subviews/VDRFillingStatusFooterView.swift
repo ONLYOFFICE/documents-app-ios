4d97b97b12eeb98dc4e7b6100ba3cec37bbae5e3
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-05-27 14:24:52 +0100

    [docspace 3.1] stop filling layout

--- Documents/Documents/Code/Controllers/Rooms/VDRFillingStatus/Subviews/VDRFillingStatusFooterView.swift
+++ Documents/Documents/Code/Controllers/Rooms/VDRFillingStatus/Subviews/VDRFillingStatusFooterView.swift
@@ -1,6 +1,6 @@
 //
 //  VDRFillingStatusFooterView.swift
-//  Documents-opensource
+//  Documents
 //
 //  Created by Pavel Chernyshev on 6.05.2025.
 //  Copyright © 2025 Ascensio System SIA. All rights reserved.
@@ -10,21 +10,21 @@ import SwiftUI
 
 /// Footer with Stop/Start actions
 struct VDRFillingStatusFooterView: View {
-    let status: VDRFillingStatusApplicationStatus?
-    let isLoading: Bool
+    let stopEnabled: Bool
+    let fillEnabled: Bool
     let onStop: () -> Void
-    let onStart: () -> Void
+    let onFill: () -> Void
 
     var body: some View {
         HStack {
-            if status == .inProgress {
-                Button("Stop filling", action: onStop)
-                    .buttonStyle(FooterButtonStyle(color: .red))
-            } else {
-                Button("Fill", action: onStart)
-                    .buttonStyle(FooterButtonStyle(color: .blue))
-                    .disabled(isLoading)
-            }
+            Button("Stop filling", action: onStop)
+                .foregroundColor(Asset.Colors.brend.swiftUIColor)
+                .buttonStyle(.borderless)
+                .disabled(!stopEnabled)
+            Spacer()
+            Button("Fill", action: onFill)
+                .buttonStyle(FooterButtonStyle(color: fillEnabled ? .blue : .secondary))
+                .disabled(!fillEnabled)
         }
         .padding()
         .background(Color.white.ignoresSafeArea(edges: .bottom))
@@ -37,8 +37,8 @@ struct FooterButtonStyle: ButtonStyle {
         configuration.label
             .font(.body)
             .foregroundColor(.white)
-            .padding(.horizontal, 24)
-            .padding(.vertical, 8)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 4)
             .background(color)
             .cornerRadius(16)
             .opacity(configuration.isPressed ? 0.7 : 1)

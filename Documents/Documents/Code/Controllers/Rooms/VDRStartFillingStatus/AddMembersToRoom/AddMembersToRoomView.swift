65edc118d2ff8c6ce836757413dc4bac8f74680e
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-08-22 17:18:40 +0100

    add members to room screen view

---
+++ Documents/Documents/Code/Controllers/Rooms/VDRStartFillingStatus/AddMembersToRoom/AddMembersToRoomView.swift
@@ -0,0 +1,104 @@
+//
+//  AddMembersToRoomView.swift
+//  Documents-develop
+//
+//  Created by Pavel Chernyshev on 22/08/25.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import Kingfisher
+import SwiftUI
+
+struct AddMembersToRoomView: View {
+    @StateObject var viewModel: AddMembersToRoomViewModel
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack {
+            header
+
+            membersList
+
+            footer
+        }
+        .background(Color(.secondarySystemBackground))
+        .onAppear { viewModel.onAppear() }
+        .navigationTitle(NSLocalizedString("Select members", comment: ""))
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(
+            trailing: Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Cancel")
+            }
+        )
+    }
+
+    private var header: some View {
+        VStack {
+            searchBar
+            segment
+        }
+    }
+
+    private var searchBar: some View {
+        SearchBar(text: $viewModel.dataModel.searchText, placeholder: "Search")
+            .padding(.horizontal, 8)
+    }
+
+    private var segment: some View {
+        Picker("Select", selection: $viewModel.dataModel.selectedSegment) {
+            ForEach(AddMembersToRoomViewModel.Segment.allCases) { segment in
+                Text(verbatim: segment.localizedString).tag(segment)
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+        .padding(.horizontal, 16)
+    }
+
+    @ViewBuilder
+    private var membersList: some View {
+        List {
+            ForEach(viewModel.screenModel.rows) { cell in
+                switch cell {
+                case let .user(model):
+                    ASCRoomTemplateMemberRow(model: .user(model))
+                case let .guest(model):
+                    ASCRoomTemplateMemberRow(model: .user(model))
+                }
+            }
+        }
+    }
+
+    private var footer: some View {
+        VStack(spacing: 0) {
+            Rectangle()
+                .fill(Color.secondary)
+                .frame(height: 0.5)
+
+            HStack {
+                Spacer()
+                Button(action: {
+                    // TODO: completion method
+                }) {
+                    Text("Add to room")
+                        .fontWeight(.semibold)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 16)
+                        .background(
+                            Asset.Colors.brend.swiftUIColor
+                        )
+                        .foregroundColor(.white)
+                        .cornerRadius(14)
+
+                    // MARK: TODO modifier
+                }
+            }
+            .padding()
+        }
+        .background(
+            Color(.systemBackground).ignoresSafeArea(edges: [.horizontal, .bottom])
+        )
+    }
+}

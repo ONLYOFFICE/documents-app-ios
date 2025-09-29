3646d084718cd4f341fa53e87f88557383bbc480
PavelChernyshev <pavel.chernyshev@onlyoffice.com>
2025-09-29 19:36:32 +0100

    mappable response model OnlyofficeSharedUser

---
+++ Documents/Documents/Code/Business/Networking/Onlyoffice/Model/OnlyofficeSharedUser.swift
@@ -0,0 +1,26 @@
+//
+//  OnlyofficeSharedUser.swift
+//  Documents-develop
+//
+//  Created by Pavel Chernyshev on 29.09.2025.
+//  Copyright © 2025 Ascensio System SIA. All rights reserved.
+//
+
+import ObjectMapper
+
+struct OnlyofficeSharedUser: Mappable {
+    
+    var name: String?
+    var email: String?
+    var image: String?
+    
+    init() {}
+
+    init?(map: Map) {}
+
+    mutating func mapping(map: Map) {
+        name <- map["name"]
+        email <- map["email"]
+        image <- map["image"]
+    }
+}
